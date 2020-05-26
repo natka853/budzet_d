71a4d98ead09ce63eaa64c7e04a456e39ae36471
milida <i.milewska@gmail.com>
2020-05-26 02:05:52 +0200

    zmiany w wyglądzie strony admina

--- Budzet/admin.py
+++ Budzet/admin.py
@@ -1,9 +1,36 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import Group
 # Register your models here.
 from .models import Dochod, Zrodlo, Wydatek, Kategoria
 
-admin.site.register(Dochod)
-admin.site.register(Zrodlo)
-admin.site.register(Wydatek)
-admin.site.register(Kategoria)
+
+class DochodAdmin(admin.ModelAdmin):
+    list_display = ('nazwa', 'zrodlo', 'opis', 'kwota', 'data')
+    list_filter = ('zrodlo', 'data')
+
+
+class WydatekAdmin(admin.ModelAdmin):
+    list_display = ('nazwa', 'kategoria', 'opis', 'kwota', 'data')
+    list_filter = ('kategoria', 'data')
+
+
+class KategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nazwa', 'user')
+    list_filter = ('user', )
+
+
+class ZrodloAdmin(admin.ModelAdmin):
+    list_display = ('nazwa', 'user')
+    list_filter = ('user', )
+
+
+admin.site.register(Dochod, DochodAdmin)
+admin.site.register(Zrodlo, ZrodloAdmin)
+admin.site.register(Wydatek, WydatekAdmin)
+admin.site.register(Kategoria, KategoriaAdmin)
+admin.site.unregister(Group)
+
+# Changing admin header
+admin.site.site_header = "Administracja budżetu domowego"
+admin.site.index_title = "Administracja budżetu"
+
