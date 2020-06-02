5d4437bcfdd63301858ee81249b1e7646379dc73
milida <i.milewska@gmail.com>
2020-06-02 10:45:10 +0200

    działający static

--- budzet_d/settings.py
+++ budzet_d/settings.py
@@ -118,10 +118,7 @@ USE_TZ = True
 STATIC_PATH = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
-STATIC_URL = '/media/'
 
 STATICFILES_DIRS = (
    STATIC_PATH,
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
