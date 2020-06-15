4b799f2bfc47d91aff9980d8e18879b6e4e21905
milida <i.milewska@gmail.com>
2020-06-15 15:00:43 +0200

    Działający test nieudanego logowania i poprawnej rejestracji

--- Budzet/views.py
+++ Budzet/views.py
@@ -382,7 +382,6 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-
             return render(request, 'users/register_success.html', {})
     else:
         form = UserRegisterForm()
